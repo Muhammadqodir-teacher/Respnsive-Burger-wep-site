0bacc0c7f51566af0d6b30e4c834752c75c8dbfc
Muhammadqodirgit <developer@example.com>
2025-06-26 23:59:50 +0500

    23:59

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav--menu');
-navToogle = document.getElementById('nav--toogle');
+navToogle = document.getElementById('nav--toggle');
 navClose = document.getElementById('nav--close');
 
 if (navToogle) {
     navToogle.addEventListener('click', () => {
         navMenu.classList.add('show-menu');
     })
-} 
+}
 
-if (navClose){
+if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu');
     })
 };
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav--menu');
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== ADD SHADOW HEADER ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/

7f7f7d61a4e0bc230729207f5d9a7805fe703ce5
Muhammadqodirgit <developer@example.com>
2025-06-26 22:48:18 +0500

    new files and header code

---
+++ script.js
@@ -0,0 +1,30 @@
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav--menu');
+navToogle = document.getElementById('nav--toogle');
+navClose = document.getElementById('nav--close');
+
+if (navToogle) {
+    navToogle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    })
+} 
+
+if (navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    })
+};
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== ADD SHADOW HEADER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
